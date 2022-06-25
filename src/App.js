ea96c90806d7e3aac8da2e5e915adad2ba491b27
abdulsagheer <abdulsagheeras29@gmail.com>
2022-06-25 19:12:33 +0530

    configured the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img
+        src="https://images.pexels.com/photos/9371782/pexels-photo-9371782.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+        width="940"
+        height="650"
+        alt="img"
+        className="avatar"
+      />
+      <canvas width="940" height="650" />
     </div>
   );
 }
